a7df2b7dbce29733b651c6fb737b2286972f9994
Diego Pacheco <diego.pacheco@iberopuebla.mx>
2026-02-03 12:37:28 -0600

    Playing with ctx.FillRect to understand

--- FirstAttempt/main.js
+++ FirstAttempt/main.js
@@ -6,7 +6,9 @@ function draw() {
         const ctx = canvas.getContext("2d");
         // so to be able to clarify the context in which we will be drawing (2d)
         ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect(10, 0, 20, 20)
+        ctx.fillRect(10, 10, 50, 50);
+        ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect(10, 50, 10, 50);
     }
     else{
         // canvas-unsupported code here
