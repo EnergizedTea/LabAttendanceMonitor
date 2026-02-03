569359e0c1a128677725cf511b8feef2f6478c6d
Diego Pacheco <diego.pacheco@iberopuebla.mx>
2026-02-03 12:20:26 -0600

    First Attempt has begun, so far canvas shows a square

---
+++ FirstAttempt/main.js
@@ -0,0 +1,16 @@
+function draw() {
+//drawing time!
+    const canvas = document.getElementById("myCanvas");
+    // we get our canvas...
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+        // so to be able to clarify the context in which we will be drawing (2d)
+        ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect(10, 0, 20, 20)
+    }
+    else{
+        // canvas-unsupported code here
+        alert("Your browser does not support canvases");
+    }
+}
+draw();
