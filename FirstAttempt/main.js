bbe960561503e565f09321757e6bde31e0fc611e
Diego Pacheco <diego.pacheco@iberopuebla.mx>
2026-02-25 14:44:05 -0600

    Simple changes for testing rescaling with css (made the square smaller)

--- FirstAttempt/main.js
+++ FirstAttempt/main.js
@@ -6,7 +6,7 @@ function draw() {
         const ctx = canvas.getContext("2d");
         // so to be able to clarify the context in which we will be drawing (2d)
         ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect(10, 10, 50, 50);
+        ctx.fillRect(3, 3, 5, 5);
     }
     else{
         // canvas-unsupported code here
