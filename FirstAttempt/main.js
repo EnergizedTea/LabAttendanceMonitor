3f0d73c50e68715d563ea66245745b823584d2ef
Diego Pacheco <diego.pacheco@iberopuebla.mx>
2026-02-25 15:03:29 -0600

    just playing around with paths

--- FirstAttempt/main.js
+++ FirstAttempt/main.js
@@ -4,25 +4,17 @@ function draw() {
     // we get our canvas...
     if (canvas.getContext) {
         const ctx = canvas.getContext("2d");
-        // so to be able to clarify the context in which we will be drawing (2d)
-        ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect(10, 10, 50, 50);
-        ctx.fillStyle = "rgb(200,0,220)";
-        ctx.fillRect(10, 1, 1, 50);
-        ctx.fillStyle = "rgb(20,100,0)";
-        ctx.fillRect(1, 50, 10, 10);
-        // ok so  (controls where on the left to start), (where to start on the top), (controls where on the right to stop), (where to start on the bottom)
-        // aka x,-x,y,-y, ergo...
-        ctx.fillStyle = "rgb(0,0,255)";
-        ctx.fillRect(5, 95, 120, 20);
-        // mmm.. but its only making rec- a of course, rect = rectangle so then
-        ctx.fillStyle = "rgb(255,255,0)";
-        // ctx.drawLine(5, 95, 120, 20);
-        // ctxstroke(); doesnt work
-        ctx.beginPath();// we declare we'll make a path (aka a line)
-        ctx.moveTo(5, 115); // here lays the starting point
-        ctx.lineTo(125, 40); // here we have the end point
-        ctx.stroke(); // aaaand go!
+        ctx.beginPath();
+        ctx.moveTo(25, 25);
+        ctx.lineTo(105, 25);
+        ctx.lineTo(25, 105);
+        ctx.fill();
+
+        ctx.moveTo(20, 125);
+        ctx.lineTo(130, 20);
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
     }
     else{
         // canvas-unsupported code here
